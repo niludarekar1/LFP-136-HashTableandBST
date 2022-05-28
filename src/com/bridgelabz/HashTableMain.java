a82d786c343fbfaf81033b2295eedbf0cdfe2334
niludarekar1 <nilu.darekar1@gmail.com>
2022-05-28 18:41:57 +0530

    Added:HashWordsFromHashTable

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -1,11 +1,22 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class HashTableMain {
     public static void main(String[] args) {
         //HashTableExp Object for Generic Type
         HashTableExp<String> hashTableExp = new HashTableExp<>();
+
+        Scanner sc = new Scanner(System.in);
+
         String str = "to be or not to be that to not be or";
+
         String strArr[] = str.split(" ");
+
+        //Integer strArr[] ={1,6,9,3,7,1,9,6,3,9};
+
+        System.out.println("Given String ="+str);
+        //Passing Array Element in HashTable Index
         for(String mystr : strArr) {
             //Get index of each object
             int index = hashTableExp.myHashFunction(mystr);
@@ -14,6 +25,14 @@ public class HashTableMain {
         }
 
         //Methode to Show Hash Table Data
+        hashTableExp.showHashList();
+
+        //Code to Remove the Given Word from HashTable Linked List
+        System.out.println("\nEnter the Word to Remove from Hash Table :");
+        String word = sc.next();
+        int index = hashTableExp.myHashFunction(word);
+        hashTableExp.removeKey(index,word);
+
         hashTableExp.showHashList();
     }
 }
